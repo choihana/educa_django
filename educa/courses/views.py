43e1a7f68350a712925fdf2d85a227d19568b62b
Joey <hanazzang999@gmail.com>
2023-08-30 10:04:46 +0900

    permission add

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from .models import Course
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 
 
 # Create your views here.
@@ -18,7 +19,7 @@ class OwnerEditMixin:
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Course
     fields = ['subject', 'title','slug','overview']
     success_url = reverse_lazy('manage_course_list')
@@ -29,13 +30,15 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
 # 강좌를 생성, 편집, 삭제하기 위한 뷰 -> 생성자만 접근 할 수 있음
 class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+    permission_required = 'courses.add_course'
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+    permission_required = 'courses.change_course'
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
 
