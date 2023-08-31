63c715696f209b2e8a84928ac37b748a667fda04
Joey <hanazzang999@gmail.com>
2023-08-31 13:33:08 +0900

    memcache

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,4 +130,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL ='/course/mine'
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
--- educa/students/urls.py
+++ educa/students/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns =[
     path('register/',views.StudentRegistrationView.as_view(), name='student_register'),
+    path('enroll-course', views.StudentEnrollCourseView.as_view(), name='student_enroll_course'),
+    path('courses/', views.StudentCourseListView.as_view(), name='student_course_list'),
+    path('courses/<pk>/', views.StudentCourseDetailView.as_view(), name='student_course_detail'),
+    path('course/<pk>/<module_id>/', views.StudentCourseDetailView.as_view(), name='student_course_detail_module'),
+
 ]
